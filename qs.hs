3e63891fbf659d79cc8d5ca002b58132fdcb79d8
Linus Arver <linusarver@gmail.com>
2011-03-27 22:11:12 -0700

    regex: remove unused functions

--- qs.hs
+++ qs.hs
@@ -346,60 +346,6 @@ abort (msg, eid) = do
 errMsg :: String -> IO ()
 errMsg msg = hPutStrLn stderr $ "error: " ++ msg
 
--- only support backreferences \0 through \9, so that we can do "\19034" to mean "backreference \1,
--- followed by '9034'"
-subRegex :: Regex                          -- ^ Search pattern
-         -> String                         -- ^ Input string
-         -> String                         -- ^ Replacement text
-         -> String                         -- ^ Output string
-subRegex _ "" _ = ""
-subRegex regexp inp repl =
-    let compile _i str [] = \ _m ->  (str++)
-        compile i str (("\\",(off,len)):rest) =
-            let i' = off+len
-                pre = take (off-i) str
-                str' = drop (i'-i) str
-            in  if null str'
-                    then \ _m -> (pre ++) . ('\\':)
-                    else \  m -> (pre ++) . ('\\' :) . compile i' str' rest m
-        compile i str ((xstr,(off,len)):rest) =
-            let i' = off+len
-                pre = take (off-i) str
-                str' = drop (i'-i) str
-                x = read xstr
-            in  if null str'
-                    then \ m -> (pre++) . ((fst (m!x))++)
-                    else \ m -> (pre++) . ((fst (m!x))++) . compile i' str' rest m
-
-        compiled :: MatchText String -> String -> String
-        compiled = compile 0 repl findrefs
-            where
-                bre = makeRegexOpts defaultCompOpt execBlank "\\\\(\\\\|[0-9])"
-                findrefs = map (\m -> (fst (m!1),snd (m!0))) (matchAllText bre repl)
-
-        go _i str [] = str
-        go i str (m:ms) =
-            let (_,(off,len)) = m!0
-                i' = off+len
-                pre = take (off-i) str
-                str' = drop (i'-i) str
-            in  if null str'
-                    then pre ++ (compiled m "")
-                    else pre ++ (compiled m (go i' str' ms))
-    in go 0 inp (matchAllText regexp inp)
-
--- Substitue re with repl in src using options copts and eopts.
-reSub :: String -> String -> String -> String
-reSub inp re repl = subRegex (makeRegexOpts copts eopts re) inp repl
-    where
-        copts = sum [compDotAll, compMultiline]
-        eopts = sum [execBlank]
-
-gsub :: String -> (String -> String) -> String -> String
-gsub re f inp = case inp =~ re :: (Int,Int) of
-    (-1,_)    -> inp
-    (begin,n) -> take begin inp ++ f (take n (drop begin inp)) ++ gsub re f (drop n (drop begin inp))
-
 makeRegexDef :: String -> Regex
 makeRegexDef re = makeRegexOpts copts eopts re
     where
