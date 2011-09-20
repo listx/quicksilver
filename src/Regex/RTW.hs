listx/quicksilver

commit 0e00c080b55f0d6af76e200ba426315fd06f839a

rtw: DS: add in descriptions of the 6 omitted settlements

src/Regex/RTW.hs
@@ -42,6 +42,7 @@ _RTW_DS_FUNCS :: RegexSets
 _RTW_DS_FUNCS = addTrueTest [r1, r2]
     ++ addTrueTest (noSpies)
     ++ mineFuncs capsSecs
+    ++ [missingSettlementDescs]
     ++ addTrueTest [giveRoads]
     ++ giveRoads'
     ++ addTrueTest goldMod
@@ -76,6 +77,46 @@ _RTW_DS_FUNCS = addTrueTest [r1, r2]
                 [ ("^ancillaries spymaster\\r\\n", nil)
                 ]
             ]
+        -- The following 6 settlements are missing from descr_strat.txt, so add
+        -- them in to build roads on them:
+        --
+        --  PROVINCE            REGION
+        --  --------            ------
+        --  Dumatha             Arabia
+        --  Phraaspa            Atropatene
+        --  Lovosice            Boihaemum
+        --  Salona              Dalmatia
+        --  Ancyra              Galatia
+        --  Domus_Dulcis_Domus  Locus_Gepidae
+        --  Vicus_Venedae       Pripet
+        missingSettlementDescs =
+                [ ("Tripolitania.+?}\\r\\n", add $ concatMap makeTown sdescs, alwaysTrue)
+                ]
+            where
+                makeTown (province, culture) =
+                    case lookup province settlementRegionTable of
+                        Just r -> "settlement\r\n{\r\n"
+                            ++ "\tlevel town\r\n"
+                            ++ "\tregion " ++ r ++ "\r\n\r\n"
+                            ++ "\tyear_founded 0\r\n"
+                            ++ "\tpopulation 2700\r\n"
+                            ++ "\tplan_set default_set\r\n"
+                            ++ "\tfaction_creator " ++ culture ++ "\r\n"
+                            ++ "\tbuilding\r\n"
+                            ++ "\t{\r\n"
+                            ++ "\t\ttype core_building governors_house\r\n"
+                            ++ "\t}\r\n"
+                            ++ "}\r\n"
+                        _ -> "" -- don't add anything if we make a human error in defining sdescs
+                sdescs =
+                    [ ("Dumatha", "parthia")
+                    , ("Phraaspa", "parthia")
+                    , ("Lovosice", "gaul")
+                    , ("Salona", "macedon")
+                    , ("Ancyra", "macedon")
+                    , ("Domus_Dulcis_Domus", "gaul")
+                    , ("Vicus_Venedae", "gaul")
+                    ]
         -- Give all settlements paved roads.
         govHouse =
             "\r\n\tbuilding\r\n\
@@ -337,7 +378,6 @@ _RTW_DS_FUNCS = addTrueTest [r1, r2]
             , ("Vicus_Marcomannii" , "Regnum_Marcomannii")
             , ("Vicus_Venedae"     , "Pripet")
             ]
-
         -- Extra elephants locations (put elephants all over northern Africa)
         elephantsMod =
                 [ ("resources.+?\\r\\n\\r\\n", addResource "elephant" elephantCoords)

src/Data/RTW.hs
@@ -26,6 +26,7 @@ readmeRTW =
     , "*** Initial provinces and buildings"
     , "- Give paved roads for all settlements (including those factions that cannot build them in vanilla)."
     , "  - All villages start out as barebone towns to allow this."
+    , "  - Also, the 6 undefined settlements (which default to villages in-game) in descr_strat.txt have been changed to barebone towns as well to allow this.."
     , "*** Agents"
     , "- Remove spies (use assassins instead; spies are over-powered anyway with their 'open gate' ability)."
     , "- Diplomats and assassins take 0 turns to recruit."

commit 8d0bc73b6d8269371fd28cf4cc0ec7f4da20b8d2

rtw: fix game-crashing typos
Fix typos introduced in commit
0e00c080b55f0d6af76e200ba426315fd06f839a.

src/Regex/RTW.hs
@@ -111,11 +111,11 @@ _RTW_DS_FUNCS = addTrueTest [r1, r2]
                 sdescs =
                     [ ("Dumatha", "parthia")
                     , ("Phraaspa", "parthia")
-                    , ("Lovosice", "gaul")
+                    , ("Lovosice", "gauls")
                     , ("Salona", "macedon")
                     , ("Ancyra", "macedon")
-                    , ("Domus_Dulcis_Domus", "gaul")
-                    , ("Vicus_Venedae", "gaul")
+                    , ("Domus_Dulcis_Domus", "gauls")
+                    , ("Vicus_Venedae", "gauls")
                     ]
         -- Give all settlements paved roads.
         govHouse =