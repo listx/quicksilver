508dbd14a9382b76ea7a7511a372462a281b667b
Linus Arver <linusarver@gmail.com>
2011-09-19 21:25:11 -0700

    zip: remove old archives

--- src/zip.sh
+++ src/zip.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 
+rm -f quicksilverM2TW.7z
+rm -f quicksilverRTW.7z
 7za a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on \
     quicksilverM2TW.7z ${1}/quicksilverM2TW/quicksilverM2TW*
 7za a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on \
