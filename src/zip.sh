f225e800b808f99aa71f199449f585523f59065f
Linus Arver <linusarver@gmail.com>
2011-09-19 19:48:14 -0700

    zip: more flexible packaging

--- src/zip.sh
+++ src/zip.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 
 7za a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on \
-    quicksilverM2TW.7z quicksilverM2TW/quicksilverM2TW*
+    quicksilverM2TW.7z ${1}/quicksilverM2TW/quicksilverM2TW*
 7za a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on \
-    quicksilverRTW.7z quicksilverRTW/quicksilverRTW*
+    quicksilverRTW.7z ${1}/quicksilverRTW/quicksilverRTW*
 
 # vim: syntax=zsh
