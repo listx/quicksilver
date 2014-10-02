89faf9bade245206c65ba543285eda44eac0d760
Linus Arver <linusarver@gmail.com>
2014-10-02 12:16:53 -0700

    zip.sh: remove vim cruft

--- src/zip.sh
+++ src/zip.sh
@@ -6,5 +6,3 @@ rm -f quicksilverRTW.7z
     quicksilverM2TW.7z ${1}/quicksilverM2TW/quicksilverM2TW*
 7za a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on \
     quicksilverRTW.7z ${1}/quicksilverRTW/quicksilverRTW*
-
-# vim: syntax=zsh
