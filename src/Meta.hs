8207b2f137e30a9c2c699d98c24c9378932ecb6e
Linus Arver <linusarver@gmail.com>
2014-10-29 15:15:33 -0700

    quicksilver 0.2.0.0

--- src/Meta.hs
+++ src/Meta.hs
@@ -2,7 +2,6 @@ module Meta where
 
 _QS_NAME, _QS_VERSION, _QS_INFO, _COPYRIGHT :: String
 _QS_NAME = "quicksilver"
-_QS_VERSION = "0.1.0.0"
+_QS_VERSION = "0.2.0.0"
 _QS_INFO = _QS_NAME ++ " version " ++ _QS_VERSION
-_COPYRIGHT = "(C) Linus Arver 2011"
-
+_COPYRIGHT = "(C) Linus Arver 2011-2014"
